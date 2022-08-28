f1ad539fbfa4fcbc4d88867b8e40fbcf10b0c2ec
VaulinDI <vdi85@mail.ru>
2022-08-28 14:53:59 +0300

    fillmplusn task solved

---
+++ Example02_FillmPlusn/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int m = 10;
+int n = 12;
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+        Console.Write("{0}\t", array[i, j]);
+    }
+    Console.WriteLine();
+}
