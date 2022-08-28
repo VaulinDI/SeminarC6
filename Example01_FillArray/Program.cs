16aad37b16c2d291e58f8717e920dd0b0d7a4eac
VaulinDI <vdi85@mail.ru>
2022-08-28 14:21:47 +0300

    fillarray task solved

---
+++ Example01_FillArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+int m = 4;
+int n = 5;
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().NextDouble() * 100;
+        Console.Write("{0,0:F1}\t", array[i, j]);
+    }
+    Console.WriteLine();
+}
